88e9e43676f03c23f3ef460eea28e6f053f6100d
Chance <ChanceMagno@gmail.com>
2017-03-24 11:14:49 -0700

    add methods/test to get members and add to team class

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 public class Team {
   private String mTeamName;
   private static List<Team> teams = new ArrayList<Team>();
+  private int mId;
+  private List<Member> mName;
 
   public Team(String teamName) {
     mTeamName = teamName;
     teams.add(this);
+    mId = teams.size();
+    mName = new ArrayList<Member>();
   }
 
   public String getTeamName() {
@@ -20,5 +24,21 @@ public class Team {
 
   public static void clear() {
     teams.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Team find(int id) {
+    return teams.get(id - 1);
+  }
+
+  public List<Member> getMembers() {
+    return mName;
+  }
+
+  public void addMember(Member name) {
+    mName.add(name);
   }
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -29,4 +29,33 @@ public class TeamTest {
     Team.clear();
     assertEquals(Team.all().size(), 0);
   }
+
+  @Test
+  public void team_InstantiatesWithAnId_1() {
+    Team newTeam = new Team("team1");
+    assertEquals(1, newTeam.getId());
+  }
+
+  @Test
+  public void find_returnsTeamWithSameId_secondTeam() {
+    Team.clear();
+    Team team1 = new Team("team1");
+    Team team2 = new Team("team2");
+    assertEquals(Team.find(team2.getId()), team2);
+  }
+
+  @Test
+  public void getMember_inititallyReturnsEmptyList_ArrayList() {
+    Team.clear();
+    Team newTeam = new Team("team1");
+    assertEquals(0, newTeam.getMembers().size());
+  }
+
+  @Test
+  public void addMember_addsMemberToTeamList_true() {
+    Team newTeam = new Team("team1");
+    Member newMember = new Member("james");
+    newTeam.addMember(newMember);
+    assertTrue(newTeam.getMembers().contains(newMember));
+  }
 }
